825712821e4f01f5f92c11c78d2899559f2c3eb7
Chandana2987 <21X31a0534@gmail.com>
2024-07-03 14:01:43 +0530

    commit message

---
+++ Corejavaprogram/src/com/tnsif/package2/Display.java
@@ -0,0 +1,14 @@
+package com.tnsif.package2;
+
+import com.tnsif.packages.Publicdemo;
+
+public class Display {
+
+	public static void main(String[] args) {
+		
+		Publicdemo pd = new Publicdemo();
+		
+		System.out.println(pd.a);
+		
+	}
+}
---
+++ Corejavaprogram/src/com/tnsif/packages/Publicdemo.java
@@ -0,0 +1,15 @@
+package com.tnsif.packages;
+
+
+public class Publicdemo {
+	 int a=80;
+	
+	public static void main(String[] args) {
+		Publicdemo pd = new Publicdemo();
+		
+		System.out.println(pd.a);
+	}
+	
+	
+
+}
